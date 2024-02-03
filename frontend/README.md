1abfb89fe7be199545bcefe644286adcacd664cb
Mohammad Hasan Heydari <hasan84heydari@gmail.com>
2024-02-03 14:16:21 +0330

    frontend added and running succesful

--- frontend/README.md
+++ frontend/README.md
@@ -1,4 +1,4 @@
-# vue-front
+# frontend
 
 ## Project setup
 ```
--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,13 +1,13 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<!--  <img alt="Vue logo" src="./assets/logo.png">-->
+  <HelloWorld />
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
+  // name: 'App',
   components: {
     HelloWorld
   }
