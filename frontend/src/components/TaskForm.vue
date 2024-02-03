b6df79f3cb19eee1d3c0a0198d289911ce2be951
erfan <erfank20041382@gmail.com>
2024-02-03 22:39:21 +0330

    complete fix the conflict issue

--- frontend/src/components/TaskForm.vue
+++ frontend/src/components/TaskForm.vue
@@ -3,11 +3,11 @@
         <h2>create a task</h2>
         <form @submit.prevent="createTask">
             <label for="title">Title:</label>
-            <input type="text" v-model="title" id="title" placeholder="Title" required/>
+            <input type="text" v-model="title" id="title" placeholder="Title" required/><br/>
             <label for="description">Description:</label>
-            <input type="text" v-model="description" id="description" placeholder="description" required/>
+            <input type="text" v-model="description" id="description" placeholder="description" required/><br/>
             <label for="completed">completed:</label>
-            <input type="checkbox" v-model="completed" id="completed" required/>
+            <input type="checkbox" v-model="completed" id="completed"/><br/>
             <button type="submit">submit</button>
         </form>
         
@@ -39,7 +39,7 @@ export default {
 
             axios.post("http://localhost:8000/api/createtask/",Task)
             .then(response=>{
-                console.log(`successfull with status ${response.status}`);
+                console.log(`task with title ${response.data.title} created`);
             })
             .catch(error=>{
                 console.log(error.data);
